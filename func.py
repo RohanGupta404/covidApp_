5eefad066a0b884b672ff8abef886df8d1b2cc79
alooboy47 <grohan123123@gmail.com>
2021-05-02 19:05:10 +0530

    added product

--- func.py
+++ func.py
@@ -17,13 +17,25 @@ def callDatabase():
 # This function will add a new user(seller) to the database
 def newUserLogin(email, password, name, phoneNumber, bio, address):
     userid = random.randint(1000000000000000, 9999999999999999)
-    newPassword = hashlib.sha256(b"rohan").hexdigest()
+    newPassword = hashlib.sha256(password).hexdigest()
 
     mycursor = mydb.cursor()
     mycursor.execute("use covidApp")
     mycursor.execute(f"INSERT INTO sellerInfo "
                      f"(userid, email, password, name, phoneNumber, bio, address) VALUES "
                      f"({userid}, '{email}', '{newPassword}', '{name}', {phoneNumber}, '{bio}', '{address}')")
+    mydb.commit()
+
+
+# This function will add a new product
+def newProduct(user_id, product_type, quantity, product_description, address):
+    product_id = random.randint(1000000000000000, 9999999999999999)
+
+    mycursor = mydb.cursor()
+    mycursor.execute("use covidApp")
+    mycursor.execute(f"INSERT INTO sellerInfo "
+                     f"(product_id, user_id, product_type, quantity, product_description, address) VALUES "
+                     f"({product_id}, {user_id}, '{product_type}', {quantity}, '{product_description}', '{address}')")
     mydb.commit()
 
 
@@ -41,41 +53,6 @@ def sendMail(recMail, message):
     server.sendmail(senderMail, recMail, message)
 
 
-import mysql.connector as A
-database = A.connect(host='localhost',database='v123',username='root',passwd='#ello')
-if database.is_connected():
-    print('Main Databse is Succesfully Connected')
-else:
-    print('Database Not Connected')
-c=database.cursor()
-try:
-    c.execute('Create Table Login_Info(ID decimal(30),User_ID decimal(30),Name char(30),Email_ID Char(30),Password char(50),Phone_No decimal(12),Bio text(500),Address text(500),Land_Mark text(50))')
-    print('Database 1 Created')
-except:
-    print('Database 1 Retrived')
-def Login_Info():
-    print('*#*#*#* Login Page *#*#*#*')
-    print()
-    n=input('ID : ')
-    p=int(input('User_ID : '))
-    Na=input('Name : ')
-    EI=input('Enter Emil ID OF The Customer : ')
-    PW=input('Password : ')
-    CID=input('Phone No : ')
-    CI=input('Bio : ')
-    C_O=input('Address : ')
-    NLM=input('Nearest Landmark : ')
-    t=(n,p,Na,EI,PW,CID,CI,C_O,NLM)
-    print("('ID','User_ID','Name','Email_ID','Password','Phone_No','Bio','Address','Land_Mark')")
-    print(t)
-    c.execute(f"Insert into Login_Info values({n},{p},{Na},{EI},{PW},{CID},{CI},{C_O},{NLM})")
-    database.commit()
-    print()
-    print('Successfully Checked-In')
-    print()
-    print('#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#')
-    print()
-Login_Info()
 
 
 
