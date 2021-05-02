5d05e1df8708a7f362ac5975fbca49f923d84b32
alooboy47 <grohan123123@gmail.com>
2021-05-02 19:17:10 +0530

    added KIVY basic

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-print('Hello World')
-print("This is Rohan")
-print('This is Shaswat')
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+from kivy.uix.button import ButtonBehavior
+from kivy.uix.image import Image
+from kivy.uix.dropdown import DropDown
+from kivy.uix.boxlayout import BoxLayout
+
+
+class HomeScreen(Screen):
+    pass
+
+
+GUI = Builder.load_file('main.kv')
+
+
+class MainApp(App):
+    def build(self):
+        return GUI
+
+
+MainApp().run()
