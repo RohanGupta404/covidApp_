b4934fbbebcf63b71664d83f73a9fa1fe34a0b01
alooboy47 <grohan123123@gmail.com>
2021-05-02 19:10:24 +0530

    added landmark

--- func.py
+++ func.py
@@ -15,27 +15,27 @@ def callDatabase():
 
 
 # This function will add a new user(seller) to the database
-def newUserLogin(email, password, name, phoneNumber, bio, address):
+def newUserLogin(email, password, name, phoneNumber, bio, address, landmark):
     userid = random.randint(1000000000000000, 9999999999999999)
     newPassword = hashlib.sha256(password).hexdigest()
 
     mycursor = mydb.cursor()
     mycursor.execute("use covidApp")
     mycursor.execute(f"INSERT INTO sellerInfo "
-                     f"(userid, email, password, name, phoneNumber, bio, address) VALUES "
-                     f"({userid}, '{email}', '{newPassword}', '{name}', {phoneNumber}, '{bio}', '{address}')")
+                     f"(userid, email, password, name, phoneNumber, bio, address, landmark) VALUES "
+                     f"({userid}, '{email}', '{newPassword}', '{name}', {phoneNumber}, '{bio}', '{address}', '{landmark}')")
     mydb.commit()
 
 
 # This function will add a new product
-def newProduct(user_id, product_type, quantity, product_description, address):
+def newProduct(user_id, product_type, quantity, product_description, address, landmark):
     product_id = random.randint(1000000000000000, 9999999999999999)
 
     mycursor = mydb.cursor()
     mycursor.execute("use covidApp")
     mycursor.execute(f"INSERT INTO sellerInfo "
-                     f"(product_id, user_id, product_type, quantity, product_description, address) VALUES "
-                     f"({product_id}, {user_id}, '{product_type}', {quantity}, '{product_description}', '{address}')")
+                     f"(product_id, user_id, product_type, quantity, product_description, address, landmark) VALUES "
+                     f"({product_id}, {user_id}, '{product_type}', {quantity}, '{product_description}', '{address}', '{landmark}')")
     mydb.commit()
 
 
